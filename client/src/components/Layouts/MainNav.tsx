767ce61345bd366dc887f99ca9ae0009a8b82869
Sascha Heimann <madcoda9000@users.noreply.github.com>
2024-01-22 11:12:12 +0100

    Fix: fixed mainMenu

--- client/src/components/Layouts/MainNav.tsx
+++ client/src/components/Layouts/MainNav.tsx
@@ -2,38 +2,9 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
-
-import { cn } from "@/lib/utils"
-import React from "react"
-import MenuEntries from "@/components/Layouts/MenuEntries"
 import { useNavigate } from "react-router-dom"
-
-const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a">>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    )
-  }
-)
-ListItem.displayName = "ListItem"
 
 export function MainNav() {
   const nav = useNavigate()
@@ -69,23 +40,16 @@ export function MainNav() {
             </span>
             <NavigationMenuContent></NavigationMenuContent>
           </NavigationMenuItem>
-          <NavigationMenuItem className="mb-[-3px] cursor-pointer">
-            <NavigationMenuTrigger
+          <NavigationMenuItem className="pl-5 cursor-pointer">
+            <span
+              onClick={() => nav("/Logs/SystemLogs")}
               className={
                 window.location.pathname.includes("Logs") ? activeLinkCss : inactiveLinkCss
               }
             >
               Logs
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[250px] gap-3 p-4 md:w-[300px] md:grid-cols-1 lg:w-[300px] ">
-                {MenuEntries.logsItems.map((component) => (
-                  <ListItem key={component.title} title={component.title} href={component.href}>
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
+            </span>
+            <NavigationMenuContent></NavigationMenuContent>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
